b9465628f8fba181b1ece86b6ac7370e178c7ff0
Luc Belliveau <luc.belliveau@nrc-cnrc.gc.ca>
2018-05-17 11:42:28 -0400

    added missing custom gc urls and views

--- concierge_theme_gc/models.py
+++ concierge_theme_gc/models.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from django.db import models
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin import ModelAdmin
+
+from core.models import User
+
+# TODO: GC difference: receives_newsletter defaults to True
 
 class AppCustomization(models.Model):
     BG_IMAGE_OPTIONS = (
@@ -20,4 +26,17 @@ class AppCustomization(models.Model):
     footer_image_left = models.FileField(null=True, blank=True)
     footer_image_right = models.FileField(null=True, blank=True)
 
+class UserAdmin(UserAdmin):
+    search_fields = ModelAdmin.search_fields = ('username', 'name', 'email',)
+    list_filter = ModelAdmin.list_filter + ('is_active', 'is_admin',)
+    list_display = ModelAdmin.list_display + ('is_active',)
+    filter_horizontal = ()
+    ordering = ('-id', )
+    fieldsets = add_fieldsets = (
+        (None, {'fields': ('last_login', 'name', 'email', 'password', 'avatar',)}),
+        ('Settings', {'fields': ('accepted_terms', 'receives_newsletter', 'is_active', 'is_admin',)}),
+    )
+
+
 admin.site.register(AppCustomization)
+# admin.site.register(User, UserAdmin)   -- can't register it twice...
